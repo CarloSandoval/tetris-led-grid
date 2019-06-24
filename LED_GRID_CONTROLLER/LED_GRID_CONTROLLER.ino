d2664cfbd2d7278e29954a82ead8f5c0ae53ffa9
CarloSandoval <carlt3ch@gmail.com>
2019-06-24 10:57:12 -0400

    creating arduino files

---
+++ LED_GRID_CONTROLLER/LED_GRID_CONTROLLER.ino
@@ -0,0 +1,26 @@
+
+/*
+  Snake config vs jumpy config
+
+  Jumpy config is the default config for displaying 
+  in a virtual array
+
+  Snake is the defaul config (optimal) when wiring
+  the led strips
+
+  the objective of this code is to transalte between
+  these 2 config.
+  
+
+*/
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
