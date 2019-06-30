125613dcc3a6de39496ab53f02a3f73b45111f56
CarloSandoval <carlt3ch@gmail.com>
2019-06-30 18:52:54 -0400

    adding basic interaction with LED grid, + portrait mode support

--- LED_GRID_CONTROLLER/LED_GRID_CONTROLLER.ino
+++ LED_GRID_CONTROLLER/LED_GRID_CONTROLLER.ino
@@ -1,44 +1,65 @@
+#include <FastLED.h>
+#define LED_PIN     5
+#define NUM_LEDS    128
+int ip =0, width = 8, height = 16, H = 15;
+CRGB leds[NUM_LEDS];
 
-/*
-  Snake config vs jumpy config
+class LED_GRID 
+{
+  public:
+  int numOfCells;
+  CRGB leds[];
 
-  Jumpy config is the default config for displaying 
-  in a virtual array
+  TETRIS_GAME (int, int);
+  
+  void display();
+  void setCell(int x, int y, CRGB color);//default to portrait config
+  void clear();//not really needed?
+};
 
-  Snake is the defaul config (optimal) when wiring
-  the led strips
+TETRIS_GAME::TETRIS_GAME(int numCells, int ledPin)
+{
+  numOfCells = numCells;
+  leds[numCells];
+  //singleton class
+  FastLED.addLeds<WS2812, ledPin, GRB>(leds, numCells);
+}
 
-  the objective of this code is to transalte between
-  these 2 config.
+void TETRIS_GAME::setCell(int x, int y, CRGB color)
+{
+  //portrait config
+  int ip = ( ((i % width)+((i % width)%2)) * H ) + (i % width)   +   ((i / width) * (int)pow(-1, (i % width)%2));
+  leds[ip] = color;
+}
 
-//display grid from jumpy config to snake config
 
-  for(int y=0, y<width; y++)
-  {
-      for(int x=0, x<width; x++)
-      {
-
-        if(y%2)//is odd
-        {
-          i = ((y+1)*width) - (x-1);
-        }
-        else
-        {
-          i = (y * width) + x;
-        }
-        
-      }
-  }
-
-*/
+void TETRIS_GAME::display()
+{
+  FastLED.show();
+}
 
 
 void setup() {
   // put your setup code here, to run once:
-
 }
-        
+
 void loop() {
   // put your main code here, to run repeatedly:
 
+  for(int i=0; i<NUM_LEDS; i++)
+  {
+    ip = ( ((i % width)+((i % width)%2)) * H ) + (i % width)   +   ((i / width) * (int)pow(-1, (i % width)%2));
+
+    ip = ip % NUM_LEDS;
+    leds[ip] = CRGB {0, 0, 255};
+    FastLED.show();
+    delay(40);
+  }
+  
+    for(int i=0; i<NUM_LEDS; i++)
+  {
+    leds[i] = CRGB {0, 255, 0};
+    FastLED.show();
+    delay(40);
+  }
 }
